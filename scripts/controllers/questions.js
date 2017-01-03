ed05c77f329aaf7188a57df50c10965b4e57343d
yomzed <martinezyoan@icloud.com>
2017-01-03 16:45:35 +0100

    Fonction d'affichage random des questions et réponses

--- scripts/controllers/questions.js
+++ scripts/controllers/questions.js
@@ -1,13 +1,20 @@
 // Contrôleur de la page des questions
-app.controller('QuestionsCtrl', function($http, $timeout) {
+app.controller('QuestionsCtrl', function($http, $scope) {
 	let ask = this;
 	let score = 0;
 	ask.questions = [];
 
 	// Récupération des questions et réponses
 	$http.get('/question.json').then(function(data) {
+		let rand = Math.ceil(Math.random()*4);
 		ask.questions = data.data;
-		console.log(ask.questions[1].question);
+		ask.demand = ask.questions[rand].question;
+		ask.answers = ask.questions[rand].reponses;
 	});
 
+	// Filtre random pour ng-repeat
+	ask.random = function() {
+		return 0.5 - Math.random();
+	}
+	
 });
