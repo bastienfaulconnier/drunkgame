7161cead07910848b7d7ce35ec8c75329634300a
yomzed <martinezyoan@icloud.com>
2017-01-04 16:12:08 +0100

    Big fonction

--- scripts/controllers/questions.js
+++ scripts/controllers/questions.js
@@ -1,7 +1,9 @@
 // Contrôleur de la page des questions
 app.controller('QuestionsCtrl', function($http, $interval, $timeout, $rootScope) {
 	let ask = this;
-	let intervalCount = 5;
+	let maxTime = 7000;
+	let maxQuestions = 5;
+	ask.timer = 100;
 	ask.score = 0;
 	ask.questions = [];
 	$rootScope.scoreIteration = 2;
@@ -13,47 +15,62 @@ app.controller('QuestionsCtrl', function($http, $interval, $timeout, $rootScope)
 		
 		// Affichage de la première question
 		let rand = Math.ceil(Math.random()*9);
-
+		maxQuestions--;
 		touillette(ask.questions[rand].reponses);
 		ask.demand = ask.questions[rand].question;
 		ask.answers = ask.questions[rand].reponses;
 		ask.questions.splice(rand, 1);
 
 		// Changement du score gagné à mi-temps
-		$timeout(ask.scoreTime, 3500);
+		var timeout = $timeout(ask.scoreTime, 3500);
 
-		// Affichage des questions suivantes au bout de 7s
-		let timer = $interval(function() {
-			console.log('Interval triggered');
-			// Reset de l'itération du score gagné
+		// Timer
+		let interval = $interval(function() {
+			ask.timer -= 100 / (maxTime / (1000/60));
+
+			if(ask.timer <= 0) {
+				ask.check('miss');
+			}
+
+		}, 1000/60);
+
+
+		// Fonction submit
+		ask.check = function(bool) {
+			ask.timer = 100;
+			$timeout.cancel(timeout);
+
+			// Ajout du score
+			if(bool == 'miss') {
+				ask.score = ask.score - 2;
+			} else if(!bool) {
+				ask.score = ask.score - 1;
+			} else if(bool) {
+				ask.score = ask.score + $rootScope.scoreIteration;
+			}
+
+			// Vérification du nb de questions restantes
+			if(maxQuestions <= 0){
+				$interval.cancel(interval);
+				ask.view = true;
+				return;
+			}
+			// Reset du gain de score
 			$rootScope.scoreIteration = 2;
 
-			rand = Math.ceil(Math.random()*ask.questions.length);
-			
-
+			// Nouvelle question
+			rand = Math.ceil(Math.random()*ask.questions.length-1);
+			maxQuestions--;
 			touillette(ask.questions[rand].reponses);
 			ask.demand = ask.questions[rand].question;
 			ask.answers = ask.questions[rand].reponses;
-			ask.questions.splice(rand, 1); // BUG ICI
-			
+			ask.questions.splice(rand, 1);
 
-			// Changement du score gagné à mi-temps
-			$timeout(ask.scoreTime, 3500);
-		}, 7000, 4);
-
-		// Affichage des résultats à la fin des questions
-		timer.then(function() {
-			ask.view = true;
-		});
+			var timeout = $timeout(ask.scoreTime, 3500);
+		}
 
 	});
 
-	// Fonction de vérification
-	ask.check = function(bool) {
-		if(bool) ask.score = ask.score + $rootScope.scoreIteration;
-		else ask.score = ask.score - 1;
-		console.log(ask.score);
-	}
 
 	// Modifier le score à mi-chemin du chrono
 	ask.scoreTime = function() {
@@ -61,8 +78,9 @@ app.controller('QuestionsCtrl', function($http, $interval, $timeout, $rootScope)
 		console.log('Iteration triggered');
 		return $rootScope.scoreIteration;
 	}
+
 	
-});
+}); // Fin controlleur
 
 // Mélanger les éléments de réponse
 function touillette(array){
