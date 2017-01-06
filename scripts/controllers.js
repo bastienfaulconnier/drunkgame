700c3e7dc9847f6b6c6d937bb75179a61f2606ac
yomzed <martinezyoan@icloud.com>
2017-01-06 12:06:18 +0100

    Review du code controller

--- scripts/controllers.js
+++ scripts/controllers.js
@@ -5,82 +5,83 @@ app.controller('ScoresCtrl', function($localStorage) {
 });
 
 // Contrôleur de la page des questions
-app.controller('QuestionsCtrl', function($http, $interval, $timeout, $rootScope, $localStorage, questions) {
+app.controller('QuestionsCtrl', function($interval, $timeout, $rootScope, $localStorage, questions) {
 	let ask = this;
+
 	let maxTime = 7000;
 	let maxQuestions = 5;
-	ask.timer = 100;
-	ask.changetimer = false;
-	ask.score = 1;
+	ask.timer = 100; // Taille de la barre chrono
+	ask.changetimer = false; // Définit la couleur de la barre
+	ask.score = 1; // Score de base
+	$rootScope.scoreIteration = 2; // Gain de point
 	ask.questions = questions.data;
-	$rootScope.scoreIteration = 2;
+
+	ask.view = false; // Définit la section affichée dans la view
+
 	$localStorage.$default({
 		players : []
 	});
-	ask.view = false;
-
-	// Récupération des questions et réponses
-	// $http.get('/question.json').then(function(data) {
-	// 	ask.questions = data.data;
+	
 		
-		// Affichage de la première question
-		let rand = Math.ceil(Math.random()*9);
+	// Affichage de la première question
+	let rand = Math.ceil(Math.random()*9);
+	maxQuestions--;
+	touillette(ask.questions[rand].reponses);
+	ask.demand = ask.questions[rand].question;
+	ask.answers = ask.questions[rand].reponses;
+	ask.questions.splice(rand, 1);
+
+
+	// Timer
+	let interval = $interval(function() {
+		ask.timer -= 100 / (maxTime / (1000/60));
+
+		// Changement du gain de point et couleur de barre à 50%
+		if(ask.timer <= 50) {
+			ask.changetimer = true;
+			$rootScope.scoreIteration = 1;
+		}
+		// Auto-submit en fin de chrono
+		if(ask.timer <= 0) {
+			ask.check('miss');
+		}
+	}, 1000/60);
+
+
+	// Fonction submit
+	ask.check = function(bool) {
+		// Reset de la barre chrono
+		ask.timer = 100; 
+		ask.changetimer = false; 
+
+		// Ajout du score
+		if(bool == 'miss') {
+			ask.score = ask.score - 2;
+		} else if(!bool) {
+			ask.score = ask.score - 1;
+		} else if(bool) {
+			ask.score = ask.score + $rootScope.scoreIteration;
+		}
+
+		// Vérification du nb de questions restantes
+		if(maxQuestions <= 0){
+			$interval.cancel(interval);
+			ask.view = true;
+			return;
+		}
+
+		$rootScope.scoreIteration = 2; // Reset du gain de score
+
+		// Nouvelle question
+		rand = Math.ceil(Math.random()*ask.questions.length-1);
 		maxQuestions--;
 		touillette(ask.questions[rand].reponses);
 		ask.demand = ask.questions[rand].question;
 		ask.answers = ask.questions[rand].reponses;
 		ask.questions.splice(rand, 1);
+	}
 
-
-		// Timer
-		let interval = $interval(function() {
-			ask.timer -= 100 / (maxTime / (1000/60));
-			if(ask.timer <= 50) {
-				ask.changetimer = true;
-				$rootScope.scoreIteration = 1;
-			}
-			if(ask.timer <= 0) {
-				ask.changetimer = false;
-				ask.check('miss');
-			}
-		}, 1000/60);
-
-
-		// Fonction submit
-		ask.check = function(bool) {
-			ask.timer = 100;
-
-			// Ajout du score
-			if(bool == 'miss') {
-				ask.score = ask.score - 2;
-			} else if(!bool) {
-				ask.score = ask.score - 1;
-			} else if(bool) {
-				ask.score = ask.score + $rootScope.scoreIteration;
-			}
-			ask.resultID = resultID(ask.score);
-			// Vérification du nb de questions restantes
-			if(maxQuestions <= 0){
-				$interval.cancel(interval);
-				ask.view = true;
-				return;
-			}
-			// Reset du gain de score
-			$rootScope.scoreIteration = 2;
-
-			// Nouvelle question
-			rand = Math.ceil(Math.random()*ask.questions.length-1);
-			maxQuestions--;
-			touillette(ask.questions[rand].reponses);
-			ask.demand = ask.questions[rand].question;
-			ask.answers = ask.questions[rand].reponses;
-			ask.questions.splice(rand, 1);
-
-		}
-
-	// });
-
-	
+	// Ecriture dans le local history
 	ask.write = function(pseudo, score) {
 		let form = {
 			"name" : pseudo,
